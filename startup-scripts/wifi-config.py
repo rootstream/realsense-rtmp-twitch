b05d5bac771efcfe0b2c5939cfca9e32a90b42b8
PieMyth <haugenw94@gmail.com>
2020-12-08 18:27:56 -0800

    improve readablility

--- startup-scripts/wifi-config.py
+++ startup-scripts/wifi-config.py
@@ -14,25 +14,29 @@ class Application(tk.Frame):
 
     def create_widgets(self):
         self.description = tk.Label(self, font=fontStyle, justify=tk.LEFT)
-        self.description["text"] = "The Depth capture kit was unable to detect internet.\n\
-Currently broadcasting a wifi hotspot to connect and setup wifi.\n\n"
+        self.description["text"] = "The Raspberry Capture Kit was unable to detect internet.\n\
+Currently broadcasting a wifi hotspot to setup wifi.\n\n"
         self.description.pack(side=tk.TOP, anchor=tk.NW)
         self.wifi_config = tk.Label(self, font=wifiStyle, justify=tk.LEFT)
         self.wifi_config["text"]= "WIFI NAME = " + self.wifiSSID + "\n\
-WIFI PASSWORD = " + self.wifiPASSWD + "\n\n"
+WIFI PASSWORD = " + self.wifiPASSWD + "\n"
         self.wifi_config.pack(side=tk.TOP, anchor=tk.NW)
         self.exit = tk.Label(self, font=fontStyle, justify=tk.LEFT)
-        self.exit["text"]= "Please connect to the wifi address above and follow the prompts on the screen.\n\
-Hit the 'QUIT' button to the bottom right of the text when done."
+        self.exit["text"]= "\
+1. Connect to the wifi name above with your device\n\
+2. Follow the prompts\n\
+3. Hit Connect\n\
+4. Tap 'NEXT' to close this screen"
         self.exit.pack(side=tk.TOP, anchor=tk.NW)
 
-        self.quit = tk.Button(self, text="QUIT", fg="red", font=fontStyle,
+        self.quit = tk.Button(self, text="NEXT", fg="red", font=buttonStyle,
                               command=self.master.destroy)
-        self.quit.pack(side=tk.TOP, anchor=tk.SE)
+        self.quit.pack(side=tk.TOP, anchor=tk.NE)
 
 root = tk.Tk()
-fontStyle = tkfont.Font(family="Lucida Grande", size=22)
-wifiStyle = tkfont.Font(family="Lucida Grande", size=34)
+fontStyle = tkfont.Font(family="Lucida Grande", size=33)
+wifiStyle = tkfont.Font(family="Lucida Grande", size=44)
+buttonStyle = tkfont.Font(family="Lucida Grande", size=85)
 root.attributes('-fullscreen', True)
 app = Application(master=root)
 app.mainloop()
