8c7f67e89a0267c2eba70272276272f920eeb25b
PieMyth <haugenw94@gmail.com>
2020-12-08 16:52:12 -0800

    readable wifi ssid + passwd

--- startup-scripts/wifi-config.py
+++ startup-scripts/wifi-config.py
@@ -13,21 +13,26 @@ class Application(tk.Frame):
         self.create_widgets()
 
     def create_widgets(self):
-        self.wifi_config = tk.Label(self, font=fontStyle, justify=tk.LEFT)
-        self.wifi_config["text"] = "The Depth capture kit was unable to detect internet.\n\
-Currently broadcasting a wifi hotspot to connect and setup wifi.\n\n\
-WIFI NAME = " + self.wifiSSID + "\n\
-WIFI PASSWORD = " + self.wifiPASSWD + "\n\n\
-Please connect to the wifi address above and follow the prompts on the screen.\n\
-Hit the 'QUIT' button to the bottom right of the text when done."
+        self.description = tk.Label(self, font=fontStyle, justify=tk.LEFT)
+        self.description["text"] = "The Depth capture kit was unable to detect internet.\n\
+Currently broadcasting a wifi hotspot to connect and setup wifi.\n\n"
+        self.description.pack(side=tk.TOP, anchor=tk.NW)
+        self.wifi_config = tk.Label(self, font=wifiStyle, justify=tk.LEFT)
+        self.wifi_config["text"]= "WIFI NAME = " + self.wifiSSID + "\n\
+WIFI PASSWORD = " + self.wifiPASSWD + "\n\n"
         self.wifi_config.pack(side=tk.TOP, anchor=tk.NW)
+        self.exit = tk.Label(self, font=fontStyle, justify=tk.LEFT)
+        self.exit["text"]= "Please connect to the wifi address above and follow the prompts on the screen.\n\
+Hit the 'QUIT' button to the bottom right of the text when done."
+        self.exit.pack(side=tk.TOP, anchor=tk.NW)
 
         self.quit = tk.Button(self, text="QUIT", fg="red", font=fontStyle,
                               command=self.master.destroy)
         self.quit.pack(side=tk.TOP, anchor=tk.SE)
 
 root = tk.Tk()
-fontStyle = tkfont.Font(family="Lucida Grande", size=20)
+fontStyle = tkfont.Font(family="Lucida Grande", size=22)
+wifiStyle = tkfont.Font(family="Lucida Grande", size=34)
 root.attributes('-fullscreen', True)
 app = Application(master=root)
 app.mainloop()
